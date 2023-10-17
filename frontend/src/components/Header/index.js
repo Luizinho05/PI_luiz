bb786d0b35a4fb926e6ca5577e45bc1a3ba33a8f
Luiz Henrique de Oliveira <luizhsimionato@gmail.com>
2023-10-17 16:58:11 -0300

    PI_luiz

---
+++ frontend/src/components/Header/index.js
@@ -0,0 +1,14 @@
+import LOGO from "../../imgs/logo.png"
+
+
+export default function Header() {
+    return (
+        <div class="container-xs">
+            <nav class="navbar navbar-dark bg-dark">
+                <a class="navbar-brand" href="/"><img src={LOGO} alt="LOGO" style={{width: 50, height: 60}}/></a>
+                <a class="navbar-brand" href="/">Vista-se! | Vestuario e identidade</a>
+                <a class="link" href="/Login">Login</a>
+            </nav>
+        </div>
+    )
+}
